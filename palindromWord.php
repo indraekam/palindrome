2be3f111822c743c60e4ed8d67950c500beef4c5
Indra Eka Mandriana <33246441+indraekam@users.noreply.github.com>
2019-05-18 22:03:48 +0700

    Update palindromWord.php

--- palindromWord.php
+++ palindromWord.php
@@ -1,1 +1,16 @@
+<?php
+class Palindrome
+{
+    public static function isPalindrome($word)
+    {
+       if(strcasecmp(strrev($word),$word)==0){
+           return true;
+       }
+        else {
+            return false;
+        } 
+    }
+}
 
+echo Palindrome::isPalindrome('Deleveled');
+?>
